2fce373f9f4d2a98aeb02d9d5e4eb7977ef549f7
johan <johan2.github@gmail.com>
2021-09-15 15:35:51 +0200

    exo 1 et 2

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"bufio"
 )
 
 func currentTime(w http.ResponseWriter, req *http.Request) {
@@ -12,9 +13,9 @@ func currentTime(w http.ResponseWriter, req *http.Request) {
 		case http.MethodGet:
 			now := time.Now()
 			heure := now.Format("15:04")
-			fmt.Println(heure)
+			fmt.Fprintf(w, heure)
 		case http.MethodPost:
-			fmt.Println("Bad method")
+			fmt.Fprintf(w, "Bad method")
 	}
 }
 
@@ -23,13 +24,38 @@ func save(w http.ResponseWriter, req *http.Request) {
 		case http.MethodGet:
 			fmt.Println("Bad method")
 		case http.MethodPost:
+			if err := req.ParseForm(); err != nil {
+				fmt.Println("Something went bad")
+				fmt.Fprintln(w, "Something went bad")
+				return
+			}
+			author := req.PostForm["author"][0]
+			entry := req.PostForm["entry"][0]
+			saveFile, err := os.OpenFile("./entries.data", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0755)
+			defer saveFile.Close()
+
+			writter := bufio.NewWriter(saveFile)
+			if err == nil {
+				fmt.Fprintf(writter, "%s:%s\n", author, entry)
+			}
+			writter.Flush()
+
+			fmt.Fprintf(w, "%s : %s\n", author, entry)
+	}
+}
+
+func entries(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+		case http.MethodGet:
 			
-		}
+		case http.MethodPost:
+		
+	}
 }
 
 func main() {
 	http.HandleFunc("/", currentTime)
 	http.HandleFunc("/add", save)
-	http.HandleFunc("/entries", helloHandler)
+	http.HandleFunc("/entries", entries)
 	http.ListenAndServe(":4567", nil)
 }
